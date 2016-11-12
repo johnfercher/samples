7a957229b0ff79bb7c5adf53c31da53c46e13bbc
sirlab <sirlab.faeterj@gmail.com>
2016-11-12 12:14:37 -0200

    added more lessons of rust

--- rust/exercises/avg.rs
+++ rust/exercises/avg.rs
@@ -14,6 +14,28 @@
  *	copies or substantial portions of the Software.
 **/
 
-fn main() {
+fn avg(notes: Vec<f32>) -> f32{
+    let mut avg : f32 = 0.0;
 
+    for i in 0..notes.len() {               // from 0 to number of elements in Vec
+        avg += notes[i];
+    }
+
+    avg = avg / (notes.len() as f32);       // NOTE: notes.len() isn't a f32. So we ahve to casting to f32
+
+    return avg;
+}
+
+fn main() {
+    let mut notes: Vec<f32> = Vec::new();   // Vec of f32 which handle the notes
+    let mut avg_note : f32;                 // avg of notes
+    
+    notes.push(8.0);
+    notes.push(8.0);
+    notes.push(6.0);
+    notes.push(5.0);
+
+    avg_note = avg(notes);
+
+    println!("{}", avg_note);
 }
