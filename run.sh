83ab25099afb96753a321a2a160a2def60ef8867
johnathan <johnathanfercher22@gmail.com>
2016-11-16 18:21:04 -0200

    update

--- run.sh
+++ run.sh
@@ -24,6 +24,7 @@ RUN_C_MM () {
     echo `./benchmark/benchmark_opt0 ./clang/exercises/matrix_multiplier_opt0 150 150 0` >> data/mm_clang.txt
     echo `./benchmark/benchmark_opt0 ./clang/exercises/matrix_multiplier_opt0 200 200 0` >> data/mm_clang.txt
     echo `./benchmark/benchmark_opt0 ./clang/exercises/matrix_multiplier_opt0 250 250 0` >> data/mm_clang.txt
+    echo `./benchmark/benchmark_opt0 ./clang/exercises/matrix_multiplier_opt0 300 300 0` >> data/mm_clang.txt
     echo " " >> data/mm_clang.txt
 
     echo "C -O1" >> data/mm_clang.txt
@@ -32,6 +33,7 @@ RUN_C_MM () {
     echo `./benchmark/benchmark_opt0 ./clang/exercises/matrix_multiplier_opt1 150 150 0` >> data/mm_clang.txt
     echo `./benchmark/benchmark_opt0 ./clang/exercises/matrix_multiplier_opt1 200 200 0` >> data/mm_clang.txt
     echo `./benchmark/benchmark_opt0 ./clang/exercises/matrix_multiplier_opt1 250 250 0` >> data/mm_clang.txt
+    echo `./benchmark/benchmark_opt0 ./clang/exercises/matrix_multiplier_opt1 300 300 0` >> data/mm_clang.txt
     echo " " >> data/mm_clang.txt
 
     echo "C -O2" >> data/mm_clang.txt
@@ -40,6 +42,7 @@ RUN_C_MM () {
     echo `./benchmark/benchmark_opt0 ./clang/exercises/matrix_multiplier_opt2 150 150 0` >> data/mm_clang.txt
     echo `./benchmark/benchmark_opt0 ./clang/exercises/matrix_multiplier_opt2 200 200 0` >> data/mm_clang.txt
     echo `./benchmark/benchmark_opt0 ./clang/exercises/matrix_multiplier_opt2 250 250 0` >> data/mm_clang.txt
+    echo `./benchmark/benchmark_opt0 ./clang/exercises/matrix_multiplier_opt2 300 300 0` >> data/mm_clang.txt
     echo " " >> data/mm_clang.txt
 
     echo "C -O3" >> data/mm_clang.txt
@@ -48,6 +51,7 @@ RUN_C_MM () {
     echo `./benchmark/benchmark_opt0 ./clang/exercises/matrix_multiplier_opt3 150 150 0` >> data/mm_clang.txt
     echo `./benchmark/benchmark_opt0 ./clang/exercises/matrix_multiplier_opt3 200 200 0` >> data/mm_clang.txt
     echo `./benchmark/benchmark_opt0 ./clang/exercises/matrix_multiplier_opt3 250 250 0` >> data/mm_clang.txt
+    echo `./benchmark/benchmark_opt0 ./clang/exercises/matrix_multiplier_opt3 300 300 0` >> data/mm_clang.txt
 }
 
 RUN_CPP_MM () {
@@ -58,6 +62,7 @@ RUN_CPP_MM () {
     echo `./benchmark/benchmark_opt0 ./cpp/exercises/matrix_multiplier_opt0 150 150 0` >> data/mm_cpp.txt
     echo `./benchmark/benchmark_opt0 ./cpp/exercises/matrix_multiplier_opt0 200 200 0` >> data/mm_cpp.txt
     echo `./benchmark/benchmark_opt0 ./cpp/exercises/matrix_multiplier_opt0 250 250 0` >> data/mm_cpp.txt
+    echo `./benchmark/benchmark_opt0 ./cpp/exercises/matrix_multiplier_opt0 300 300 0` >> data/mm_cpp.txt
     echo " " >> data/mm_cpp.txt
 
     echo "CPP -O1" >> data/mm_cpp.txt
@@ -66,6 +71,7 @@ RUN_CPP_MM () {
     echo `./benchmark/benchmark_opt0 ./cpp/exercises/matrix_multiplier_opt1 150 150 0` >> data/mm_cpp.txt
     echo `./benchmark/benchmark_opt0 ./cpp/exercises/matrix_multiplier_opt1 200 200 0` >> data/mm_cpp.txt
     echo `./benchmark/benchmark_opt0 ./cpp/exercises/matrix_multiplier_opt1 250 250 0` >> data/mm_cpp.txt
+    echo `./benchmark/benchmark_opt0 ./cpp/exercises/matrix_multiplier_opt1 300 300 0` >> data/mm_cpp.txt
     echo " " >> data/mm_cpp.txt
     
     echo "CPP -O2" >> data/mm_cpp.txt
@@ -74,6 +80,7 @@ RUN_CPP_MM () {
     echo `./benchmark/benchmark_opt0 ./cpp/exercises/matrix_multiplier_opt2 150 150 0` >> data/mm_cpp.txt
     echo `./benchmark/benchmark_opt0 ./cpp/exercises/matrix_multiplier_opt2 200 200 0` >> data/mm_cpp.txt
     echo `./benchmark/benchmark_opt0 ./cpp/exercises/matrix_multiplier_opt2 250 250 0` >> data/mm_cpp.txt
+    echo `./benchmark/benchmark_opt0 ./cpp/exercises/matrix_multiplier_opt2 300 300 0` >> data/mm_cpp.txt
     echo " " >> data/mm_cpp.txt
 
     echo "CPP -O3" >> data/mm_cpp.txt
@@ -82,6 +89,7 @@ RUN_CPP_MM () {
     echo `./benchmark/benchmark_opt0 ./cpp/exercises/matrix_multiplier_opt3 150 150 0` >> data/mm_cpp.txt
     echo `./benchmark/benchmark_opt0 ./cpp/exercises/matrix_multiplier_opt3 200 200 0` >> data/mm_cpp.txt
     echo `./benchmark/benchmark_opt0 ./cpp/exercises/matrix_multiplier_opt3 250 250 0` >> data/mm_cpp.txt
+    echo `./benchmark/benchmark_opt0 ./cpp/exercises/matrix_multiplier_opt3 300 300 0` >> data/mm_cpp.txt
 }
 
 RUN_CSHARP_MM () {
@@ -92,6 +100,7 @@ RUN_CSHARP_MM () {
     echo `./benchmark/benchmark_opt0 ./csharp/exercises/matrix_multiplier_opt0 150 150 0` >> data/mm_csharp.txt
     echo `./benchmark/benchmark_opt0 ./csharp/exercises/matrix_multiplier_opt0 200 200 0` >> data/mm_csharp.txt
     echo `./benchmark/benchmark_opt0 ./csharp/exercises/matrix_multiplier_opt0 250 250 0` >> data/mm_csharp.txt
+    echo `./benchmark/benchmark_opt0 ./csharp/exercises/matrix_multiplier_opt0 300 300 0` >> data/mm_csharp.txt
     echo " " >> data/mm_csharp.txt
 
     echo "CSHARP -optimize" >> data/mm_csharp.txt
@@ -100,6 +109,7 @@ RUN_CSHARP_MM () {
     echo `./benchmark/benchmark_opt0 ./csharp/exercises/matrix_multiplier_opt1 150 150 0` >> data/mm_csharp.txt
     echo `./benchmark/benchmark_opt0 ./csharp/exercises/matrix_multiplier_opt1 200 200 0` >> data/mm_csharp.txt
     echo `./benchmark/benchmark_opt0 ./csharp/exercises/matrix_multiplier_opt1 250 250 0` >> data/mm_csharp.txt
+    echo `./benchmark/benchmark_opt0 ./csharp/exercises/matrix_multiplier_opt1 300 300 0` >> data/mm_csharp.txt
 }
 
 RUN_RUST_MM () {
@@ -110,6 +120,7 @@ RUN_RUST_MM () {
     echo `./benchmark/benchmark_opt0 ./rust/exercises/matrix_multiplier/target/release/matrix_multiplier 150 150 0` >> data/mm_rust.txt
     echo `./benchmark/benchmark_opt0 ./rust/exercises/matrix_multiplier/target/release/matrix_multiplier 200 200 0` >> data/mm_rust.txt
     echo `./benchmark/benchmark_opt0 ./rust/exercises/matrix_multiplier/target/release/matrix_multiplier 250 250 0` >> data/mm_rust.txt
+    echo `./benchmark/benchmark_opt0 ./rust/exercises/matrix_multiplier/target/release/matrix_multiplier 300 300 0` >> data/mm_rust.txt
 }
 
 RUN_LUA_MM () {
@@ -120,6 +131,7 @@ RUN_LUA_MM () {
     echo `./benchmark/benchmark_opt0 lua lua/exercises/matrix_multiplier.lua 150 150` >> data/mm_lua.txt
     echo `./benchmark/benchmark_opt0 lua lua/exercises/matrix_multiplier.lua 200 200` >> data/mm_lua.txt
     echo `./benchmark/benchmark_opt0 lua lua/exercises/matrix_multiplier.lua 250 250` >> data/mm_lua.txt
+    echo `./benchmark/benchmark_opt0 lua lua/exercises/matrix_multiplier.lua 300 300` >> data/mm_lua.txt
 }
 
 RUN_PHP_MM () {
@@ -130,6 +142,7 @@ RUN_PHP_MM () {
     echo `./benchmark/benchmark_opt0 php php/exercises/matrix_multiplier.php 150 150` >> data/mm_php.txt
     echo `./benchmark/benchmark_opt0 php php/exercises/matrix_multiplier.php 200 200` >> data/mm_php.txt
     echo `./benchmark/benchmark_opt0 php php/exercises/matrix_multiplier.php 250 250` >> data/mm_php.txt
+    echo `./benchmark/benchmark_opt0 php php/exercises/matrix_multiplier.php 300 300` >> data/mm_php.txt
 }
 
 RUN_JAVA_MM () {
@@ -141,6 +154,7 @@ RUN_JAVA_MM () {
     echo `./../../benchmark/benchmark_opt0 java matrix_multiplier 150 150` >> ../../data/mm_java.txt
     echo `./../../benchmark/benchmark_opt0 java matrix_multiplier 200 200` >> ../../data/mm_java.txt
     echo `./../../benchmark/benchmark_opt0 java matrix_multiplier 250 250` >> ../../data/mm_java.txt
+    echo `./../../benchmark/benchmark_opt0 java matrix_multiplier 300 300` >> ../../data/mm_java.txt
     cd ..
     cd ..
 }
