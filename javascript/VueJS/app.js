ff5a8109233b39c30462423b7d8811577a87d217
johnathan <johnathanfercher22@gmail.com>
2017-07-15 15:44:11 -0300

    VueJS - Aula 4 - Methods

--- javascript/VueJS/app.js
+++ javascript/VueJS/app.js
@@ -1,3 +1,13 @@
 new Vue({
-
+  el: "#vue-app",
+  data: {
+    name: "Shaun",
+    job: "Ninja",
+    website: "www.facebook.com",
+  },
+  methods:{
+    greet: function (msg) {
+      return "Good " + msg + ", " + this.name + ".";
+    }
+  },
 });
