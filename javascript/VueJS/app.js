e5446c6b00c77c7bb318e627f6d3a62e74ca3ac2
johnathan <johnathanfercher22@gmail.com>
2017-07-15 19:44:48 -0300

    VueJS - Aula 12 - Loops

--- javascript/VueJS/app.js
+++ javascript/VueJS/app.js
@@ -3,7 +3,13 @@ new Vue({
   data: {
     name: "john",
     error: false,
-    success: false
+    success: false,
+    characters: ["Mario", "Luigi", "Yoshi", "Bowser"],
+    ninjas:[
+      {name: "Ryu", age: 25},
+      {name: "Yoshi", age: 35},
+      {name: "Ken", age: 55}
+    ]
   },
   methods:{
 
