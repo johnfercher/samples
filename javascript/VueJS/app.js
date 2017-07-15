bb01c6b70ad9ea791b93969cf6ff19bdeed8c0b8
johnathan <johnathanfercher22@gmail.com>
2017-07-15 16:29:37 -0300

    VueJS - Aula 5 - Events

--- javascript/VueJS/app.js
+++ javascript/VueJS/app.js
@@ -4,10 +4,25 @@ new Vue({
     name: "Shaun",
     job: "Ninja",
     website: "www.facebook.com",
+    websiteTag:"",
+    age: 23,
+    x: 0,
+    y: 0
   },
   methods:{
     greet: function (msg) {
       return "Good " + msg + ", " + this.name + ".";
+    },
+    // Events
+    OnAdd : function (plus) {
+      this.age += plus;
+    },
+    OnSubtract : function (minus) {
+      this.age -= minus;;
+    },
+    OnUpdateXY : function (event) {
+      this.x = event.offsetX;
+      this.y = event.offsetY;
     }
   },
 });
