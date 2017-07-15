357ff1ffb300b2fa402912291d1c7c26b9532d5c
johnathan <johnathanfercher22@gmail.com>
2017-07-15 19:00:25 -0300

    VueJS - Aula 11 - Conditionals

--- javascript/VueJS/app.js
+++ javascript/VueJS/app.js
@@ -1,55 +1,14 @@
 new Vue({
   el: "#vue-app",
   data: {
-    name: "Shaun",
-    job: "Ninja",
-    website: "www.facebook.com",
-    websiteTag:"",
-    age: 23,
-    x: 0,
-    y: 0,
-    a: 0,
-    b: 0,
-    available: false,
-    nearby: false,
+    name: "john",
+    error: false,
+    success: false
   },
   methods:{
-    greet: function (msg) {
-      return "Good " + msg + ", " + this.name + ".";
-    },
-    // Events
-    OnAdd : function (plus) {
-      this.age += plus;
-    },
-    OnSubtract : function (minus) {
-      this.age -= minus;;
-    },
-    OnUpdateXY : function (event) {
-      this.x = event.offsetX;
-      this.y = event.offsetY;
-    },
-    OnClick : function () {
-      alert("Clickkk");
-    },
-    OnAgeCheck : function () {
-      this.age = event.target.value;
-    },
 
   },
   computed:{
-    addToA : function () {
-      console.log("A");
-      return this.a + this.age;
-    },
-    addToB : function () {
-      console.log("B");
-      return this.b + this.age;
-    },
-    compClasses : function () {
-      return {
-        available: this.available,
-        nearby: this.nearby
-      }
-    }
+
   },
 });
