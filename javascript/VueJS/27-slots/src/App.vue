42de7ad643d716373ba1c8a87db186e310ed6b8b
johnathan <johnathanfercher22@gmail.com>
2017-07-16 13:48:11 -0300

    VueJS - Aula 27 - Slots

---
+++ javascript/VueJS/27-slots/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <form-helper>
+      <div slot="form-header">
+        <h3>One title</h3>
+        <p>information</p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="name" required/>
+        <input type="password" placeholder="password" required/>
+      </div>
+      <div slot="form-controls">
+        <button @click="handleSubmit">Submit</button>
+      </div>
+    </form-helper>
+  </div>
+</template>
+
+<script>
+import formHelper from "./components/formHelper.vue";
+export default {
+  components:{
+    'form-helper': formHelper
+  },
+  data () {
+    return {
+      title: "dynamic"
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ javascript/VueJS/27-slots/src/components/formHelper.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>fill the form mf</h1>
+    <form>
+      <div id="form-header">
+        <slot name="form-header"></slot>
+      </div>
+      <div id="form-fields">
+        <slot name="form-fields"></slot>
+      </div>
+      <div id="form-controls">
+        <slot name="form-controls"></slot>
+      </div>
+      <div id="useful-links">
+        <ul>
+          <li><a href="#">Link</a></li>
+          <li><a href="#">Link</a></li>
+          <li><a href="#">Link</a></li>
+          <li><a href="#">Link</a></li>
+        </ul>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  components:{
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
