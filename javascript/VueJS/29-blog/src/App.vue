0b6ff620bb386cb775a4aef52ea1ffb8912d8014
johnathan <johnathanfercher22@gmail.com>
2017-07-17 13:47:07 -0300

    VueJS - Aula 38 - Mixins

--- javascript/VueJS/29-blog/src/App.vue
+++ javascript/VueJS/29-blog/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <add-blog></add-blog>
+    <list-blogs></list-blogs>
     <show-blogs></show-blogs>
   </div>
 </template>
@@ -8,11 +9,13 @@
 <script>
 import addBlog from "./components/addBlog.vue";
 import showBlogs from "./components/showBlogs.vue"
+import listBlogs from "./components/listBlogs.vue"
 
 export default {
   components:{
     'add-blog': addBlog,
-    'show-blogs': showBlogs
+    'show-blogs': showBlogs,
+    'list-blogs': listBlogs
   },
   data () {
     return {
--- javascript/VueJS/29-blog/src/components/listBlogs.vue
+++ javascript/VueJS/29-blog/src/components/listBlogs.vue
@@ -1,10 +1,9 @@
 <template>
   <div v-theme:column="'narrow'" id="show-blogs">
-    <h1>All blogs</h1>
+    <h1>All blogs titles</h1>
     <input type="text" v-model="search" class="search" placeholder="search blogs"/>
     <div v-for="blog in filteredBlogs" class="single-blog">
       <h2 v-rainbow>{{blog.title | to-uppercase}}</h2>
-      <article>{{blog.body | snippet}}</article>
     </div>
   </div>
 </template>
@@ -62,7 +61,7 @@ export default {
       }
     }
   },
-  // mixins
+  // Mixins
   mixins: [searchMixin]
 }
 </script>
