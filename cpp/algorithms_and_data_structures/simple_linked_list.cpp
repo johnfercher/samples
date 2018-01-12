868b05effff0468d63aca545a2a29e97cc916e3a
John Fercher <johnathanfercher22@gmail.com>
2018-01-11 23:17:08 -0200

    array

--- cpp/algorithms_and_data_structures/simple_linked_list.cpp
+++ cpp/algorithms_and_data_structures/simple_linked_list.cpp
@@ -22,21 +22,29 @@ class LinkedList{
 private:
   Node<T> *head;
 
-  int deepSearch(Node<T> *actualNode, int actualSize = 0){
+  int deep_search(Node<T> *actualNode, int actualSize = 0){
     if(actualNode == NULL)
       return actualSize;
 
-    deepSearch(actualNode->next, ++actualSize);
+    deep_search(actualNode->next, ++actualSize);
   }
 
-  std::optional<T> recursiveElementSearch(Node<T> *actualNode, int objectiveLevel, int actualLevel = 0){
+  std::optional<T> recursive_element_search(Node<T> *actualNode, int objectiveLevel, int actualLevel = 0){
     if(actualNode == NULL)
       return {};
 
     if(actualLevel == objectiveLevel)
       return actualNode->value;
 
-    return recursiveElementSearch(actualNode->next, objectiveLevel, ++actualLevel);
+    return recursive_element_search(actualNode->next, objectiveLevel, ++actualLevel);
+  }
+
+  void fill_array(Node<T> *actualNode, T *array, int actualIndex = 0){
+    if(actualNode == NULL)
+      return;
+
+    array[actualIndex] = actualNode->value;
+    fill_array(actualNode->next, array, ++actualIndex);
   }
 
 public:
@@ -44,14 +52,14 @@ public:
     head = NULL;
   }
 
-  void push(int value){
+  void push_back(int value){
     Node<T> *n = new Node<T>();
     n->value = value;
     n->next = head;
     head = n;
   }
 
-  std::optional<T> pop(){
+  std::optional<T> pop_back(){
     if(head == NULL)
       return {};
 
@@ -63,12 +71,15 @@ public:
     return ret;
   }
 
-  std::optional<T> peek(){
-    return head == NULL ? {} : head->value;
+  std::optional<T> peek_back(){
+    if(head == NULL)
+      return {};
+
+    return head->value;
   }
 
   std::optional<T> at(int index){
-    if(auto element = recursiveElementSearch(head, index))
+    if(auto element = recursive_element_search(head, index))
         return *element;
 
     return {};
@@ -79,7 +90,13 @@ public:
   }
 
   int size(){
-    return deepSearch(head);
+    return deep_search(head);
+  }
+
+  T* to_array(){
+    T *array = new T[size()];
+    fill_array(head, array);
+    return array;
   }
 };
 
@@ -88,14 +105,15 @@ int main(){
 
   cout << list.any() << endl;
 
-  list.push(12);
-  list.push(55);
-  list.push(16);
+  list.push_back(12);
+  list.push_back(55);
+  list.push_back(16);
+
 
   cout << list.any() << endl;
   cout << list.at(3).value_or(0) << endl;
 
-  cout << list.pop().value_or(0) << endl;
-  cout << list.pop().value_or(0) << endl;
-  cout << list.pop().value_or(0) << endl;
+  cout << list.pop_back().value_or(0) << endl;
+  cout << list.pop_back().value_or(0) << endl;
+  cout << list.pop_back().value_or(0) << endl;
 }
